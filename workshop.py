8e79ec9b516605a00e9db6502eca93da3b808eb2
nourrysebastieN <sebastien.nourry@epitech.eu>
2021-11-18 15:47:14 +0400

    on my way to learn python

---
+++ workshop.py
@@ -0,0 +1,33 @@
+print("step0:")
+
+print("success")
+
+print("step1.0:")
+
+fruit = "banana"
+quantity = 3
+pie_crust = "empty"
+isOvenOn = False
+
+print(f"You have {quantity} {fruit} and the pie crust is {pie_crust}, is the oven on? {isOvenOn}")
+
+print("step1.1:")
+
+fruit = "apple"
+quantity = 5
+pie_crust = "empty"
+isOvenOn = True
+
+print(f"You have {quantity} {fruit} and the pie crust is {pie_crust}, is the oven on? {isOvenOn}")
+
+print("step1.2:")
+
+def print_hello_please() :
+    print("Hello")
+
+print_hello_please()
+
+def print_hello_with_my_name_please(myName) :
+    print("Hello", myName)
+
+print_hello_with_my_name_please("Sébastien")
